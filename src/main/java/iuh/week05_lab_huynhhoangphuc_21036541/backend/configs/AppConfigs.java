bac03b403a9d49a45b875b4a5e6aa870af5572a7
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-12-02 13:30:30 +0700

    update configs

--- src/main/java/iuh/week05_lab_huynhhoangphuc_21036541/backend/configs/AppConfigs.java
+++ src/main/java/iuh/week05_lab_huynhhoangphuc_21036541/backend/configs/AppConfigs.java
@@ -1,4 +1,14 @@
 package iuh.week05_lab_huynhhoangphuc_21036541.backend.configs;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfigs {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
